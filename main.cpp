7a0cfc882e43dfde91d451c81325acfdd1950b21
Daniel Subomi <subbydanny38@gmail.com>
2022-05-15 21:45:24 -0700

    initial commit
    Finished the assignment and made changes to two of the function parameters

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ using std::ostream;
 unsigned long long int Fibonacci(unsigned int n);
 void PrintReverseString(const string& str, ostream& output = cout);
 // You may change the parameters of these functions
-size_t MinimumPosition(const int array[], size_t size);
+size_t MinimumPosition(const int array[], size_t size, size_t position = 0, size_t minPos = 0);
 void SelectionSort(int array[], size_t size);
+
 
 
 int main() {
@@ -85,14 +86,48 @@ int main() {
 }
 
 unsigned long long int Fibonacci(unsigned int n){
-	return 0;
+    if(n <= 1){
+        return n;
+    }
+
+    return Fibonacci(n-1) + Fibonacci(n-2);
 }
 void PrintReverseString(const string& str, ostream& output){
+    if (str.size() == 0) {
+        return;
+    } else {
+        size_t Chars = str.size();
+
+        if (Chars == 1) {
+            output << str ;
+        } else {
+            output << str[Chars - 1];
+            PrintReverseString(str.substr(0, Chars - 1),output);
+        }
+    }
 }
 // You may change the parameters of these functions
-size_t MinimumPosition(const int array[], size_t size){
-	return 0;
+size_t MinimumPosition(const int array[], size_t size, size_t position, size_t minPos){
+    if(position > size)
+        return 1;
+    if(position == size)
+        return minPos;
+    else
+    if (array[position] < array[minPos])
+        return MinimumPosition(array, size, position + 1, position);
+    else
+        return MinimumPosition(array, size, position + 1, minPos);
+
 }
 void SelectionSort(int array[], size_t size){
-
+    int i, j, min_num;
+    for (i = 0; i < size-1; i++){
+        min_num = i;
+        for (j = i+1; j < size; j++)
+            if (array[j] < array[min_num])
+                min_num = j;
+          int temp = array[i];
+          array[i] = array[min_num];
+          array[min_num] = temp;
+    }
 }
